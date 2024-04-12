758bc793e923ad16e85e11299ff4a0f4657de648
= <boy.krijnen@ziggo.nl>
2024-04-12 09:53:36 +0200

    Deleted bot for new repo

--- api/flask.py
+++
@@ -1,8 +0,0 @@
-import os, flask
-
-class Flask():
-    def __init__(self, client):
-        self.client = client
-        self.port = os.getenv('flask.port')
-        self.dev = os.getenv('dev.enabled') 
-        print(self.client)
